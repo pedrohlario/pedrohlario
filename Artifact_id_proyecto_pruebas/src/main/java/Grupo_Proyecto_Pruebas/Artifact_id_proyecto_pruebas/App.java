d0106bd0721df44ea9d0a2a1020032da76c7a3e0
pedro <pedro@172.22.77.79>
2019-10-21 16:10:36 +0200

    Nuevo Proyecto

---
+++ Artifact_id_proyecto_pruebas/src/main/java/Grupo_Proyecto_Pruebas/Artifact_id_proyecto_pruebas/App.java
@@ -0,0 +1,13 @@
+package Grupo_Proyecto_Pruebas.Artifact_id_proyecto_pruebas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
