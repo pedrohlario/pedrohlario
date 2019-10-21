88014b8711b655ec10653adcabb3aec0855ea1b7
Pedro Portatil <pedroh.lariomonte@gmail.com>
2019-10-21 16:30:21 +0200

    commit true

--- Artifact_id_proyecto_pruebas/src/main/java/Grupo_Proyecto_Pruebas/Artifact_id_proyecto_pruebas/App.java
+++ Artifact_id_proyecto_pruebas/src/main/java/Grupo_Proyecto_Pruebas/Artifact_id_proyecto_pruebas/App.java
@@ -9,6 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World2!" );        
+        System.out.println( "Hello World2!" );  
+        if (true) {
+        	System.out.println(" holi holi");
+        }
     }
 }
